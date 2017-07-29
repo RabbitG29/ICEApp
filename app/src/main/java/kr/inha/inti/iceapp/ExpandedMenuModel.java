996b6e43afd4c7a39d6dd9a1c1745e9b086f8411
RabbitG29 <recoder1212@naver.com>
2017-07-29 22:10:58 +0900

    1. replace Navigation's list to expandable list

---
+++ app/src/main/java/kr/inha/inti/iceapp/ExpandedMenuModel.java
@@ -0,0 +1,24 @@
+package kr.inha.inti.iceapp;
+
+/**
+ * Created by KDH on 2017-07-29.
+ */
+
+public class ExpandedMenuModel {
+
+    String iconName = "";
+    int iconImg = -1; // menu icon resource id
+
+    public String getIconName() {
+        return iconName;
+    }
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+   /* public int getIconImg() {
+        return iconImg;
+    }
+    public void setIconImg(int iconImg) {
+        this.iconImg = iconImg;
+    }*/
+}
--- app/src/main/java/kr/inha/inti/iceapp/MainActivity.java
+++ app/src/main/java/kr/inha/inti/iceapp/MainActivity.java
@@ -3,9 +3,11 @@ package kr.inha.inti.iceapp;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,20 +23,41 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.AdapterViewFlipper;
 import android.widget.BaseAdapter;
+//import android.widget.ExpandableListView;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import android.support.design.widget.NavigationView;
 
 import static kr.inha.inti.iceapp.R.id.fab;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+         {
     private long lastTimeBackPressed; // 뒤로 두번 누를 때의 간격
     List<Integer> galleryId = new ArrayList<Integer>(); // 이미지 플리퍼를 위한 이미지들을 리스트형으로 선언
     AdapterViewFlipper avf; // 이미지 플리퍼
+    private DrawerLayout mDrawerLayout;
+    ExpandableListAdapter mMenuAdapter;
+    ExpandableListView expandableList;
+    List<ExpandedMenuModel> listDataHeader;
+    HashMap<ExpandedMenuModel, List<String>> listDataChild;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +65,41 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        /* to set the menu icon image*/
+        ab.setHomeAsUpIndicator(android.R.drawable.ic_menu_add);
+        ab.setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        expandableList = (ExpandableListView) findViewById(R.id.navigationmenu);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        for (int i=1; i<6;i++) {
-            galleryId.add(getResources().getIdentifier("t"+i,"drawable",this.getPackageName()));
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        prepareListData();
+        mMenuAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild, expandableList);
+
+        // setting list adapter
+        expandableList.setAdapter(mMenuAdapter);
+
+        expandableList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                //Log.d("DEBUG", "submenu item clicked");
+                return false;
+            }
+        });
+        expandableList.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+                //Log.d("DEBUG", "heading clicked");
+                return false;
+            }
+        });
+
+        for (int i = 1; i < 6; i++) {
+            galleryId.add(getResources().getIdentifier("t" + i, "drawable", this.getPackageName()));
         } // t1부터 t6까지 이미지 띄움
 
         avf = (AdapterViewFlipper) findViewById(R.id.image_flips);
@@ -66,8 +121,12 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -80,8 +139,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else {
+        } else {
             if (System.currentTimeMillis() - lastTimeBackPressed < 1500) {
                 finish();
                 return;
@@ -92,6 +150,42 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
    /*  private void verifyDialog(){
         Log.e("response","Verify!!");
         LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -99,11 +193,9 @@ public class MainActivity extends AppCompatActivity
         AlertDialog.Builder adb = new AlertDialog.Builder(this);
         adb.setTitle("학번 인증");
         adb.setView(verifyLayout);
-
         adb.setNeutralButton("OK",new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) { // 이 부분에 OK를 눌렀을 때 어떻게 되는지를 추가
-
             }
         }).show();
     } */
@@ -114,11 +206,11 @@ public class MainActivity extends AppCompatActivity
         LayoutInflater inflater;
 
         public galleryAdapter(Context c) {
-            mContext=c;
+            mContext = c;
             inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
-        public int getCount(){
+        public int getCount() {
             return galleryId.size();
         }
 
@@ -132,7 +224,7 @@ public class MainActivity extends AppCompatActivity
 
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView=inflater.inflate(R.layout.item,parent,false);
+                convertView = inflater.inflate(R.layout.item, parent, false);
             }
             ImageView imageView = (ImageView) convertView.findViewById(R.id.items);
             imageView.setImageResource(galleryId.get(position));
@@ -142,7 +234,7 @@ public class MainActivity extends AppCompatActivity
 
 
     @SuppressWarnings("StatementWithEmptyBody")
-    @Override
+    /*@Override
     public boolean onNavigationItemSelected(MenuItem item) { // 각각의 버튼을 눌렀을 때 어떻게 동작하는지
         // Handle navigation view item clicks here.
         int id = item.getItemId();
@@ -172,5 +264,162 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }*/
+
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<ExpandedMenuModel>();
+        listDataChild = new HashMap<ExpandedMenuModel, List<String>>();
+
+        ExpandedMenuModel item1 = new ExpandedMenuModel();
+        item1.setIconName("학과소개");
+        // Adding data header
+        listDataHeader.add(item1);
+
+        ExpandedMenuModel item2 = new ExpandedMenuModel();
+        item2.setIconName("교수 및 연구");
+        listDataHeader.add(item2);
+
+        ExpandedMenuModel item3 = new ExpandedMenuModel();
+        item3.setIconName("학사안내");
+        listDataHeader.add(item3);
+
+        ExpandedMenuModel item4 = new ExpandedMenuModel();
+        item4.setIconName("교과안내");
+        listDataHeader.add(item4);
+
+
+        ExpandedMenuModel item5 = new ExpandedMenuModel();
+        item5.setIconName("학생활동");
+        listDataHeader.add(item5);
+
+        ExpandedMenuModel item6 = new ExpandedMenuModel();
+        item6.setIconName("정보통신 종합설계");
+        listDataHeader.add(item6);
+
+        ExpandedMenuModel item7 = new ExpandedMenuModel();
+        item7.setIconName("정보마당");
+        listDataHeader.add(item7);
+
+        ExpandedMenuModel item8 = new ExpandedMenuModel();
+        item8.setIconName("세미나실 예약");
+        listDataHeader.add(item8);
+
+        // Adding child data
+        List<String> heading1 = new ArrayList<String>();
+        heading1.add("인사말");
+        heading1.add("교육목표 및 연혁");
+        heading1.add("교육 시설");
+        heading1.add("졸업 후 진로");
+        heading1.add("위치 및 연락처");
+
+        List<String> heading2 = new ArrayList<String>();
+        heading2.add("교수 소개");
+        heading2.add("연구실 소개");
+
+        List<String> heading3 = new ArrayList<String>();
+        heading3.add("학사일정");
+        heading3.add("졸업요건");
+        heading3.add("장학금 안내");
+
+        List<String> heading4 = new ArrayList<String>();
+        heading4.add("교육과정");
+        heading4.add("ABEEK");
+        heading4.add("이수체계도");
+
+        List<String> heading5 = new ArrayList<String>();
+        heading5.add("자유게시판");
+        heading5.add("학생회");
+        heading5.add("소모임");
+        heading5.add("사진으로 보는 ICE");
+
+        List<String> heading6 = new ArrayList<String>();
+        heading6.add("공지사항");
+        heading6.add("팀원모집");
+        heading6.add("연도별 논문");
+
+        List<String> heading7 = new ArrayList<String>();
+        heading7.add("학과 공지사항");
+        heading7.add("대학원 공지사항");
+        heading7.add("FAQ");
+        heading7.add("취업정보");
+
+        List<String> heading8 = new ArrayList<String>();
+        heading8.add("ICE Cafe");
+        heading8.add("대학원생 세미나실");
+
+
+        listDataChild.put(listDataHeader.get(0), heading1);// Header, Child data
+        listDataChild.put(listDataHeader.get(1), heading2);
+        listDataChild.put(listDataHeader.get(2), heading3);
+        listDataChild.put(listDataHeader.get(3), heading4);
+        listDataChild.put(listDataHeader.get(4), heading5);
+        listDataChild.put(listDataHeader.get(5), heading6);
+        listDataChild.put(listDataHeader.get(6), heading7);
+        listDataChild.put(listDataHeader.get(7), heading8);
+
+       /* ExpandedMenuModel item1 = new ExpandedMenuModel();
+        item1.setIconName("학과소개");
+        item1.setIconImg(android.R.drawable.ic_delete);
+        // Adding data header
+        listDataHeader.add(item1);
+
+        ExpandedMenuModel item2 = new ExpandedMenuModel();
+        item2.setIconName("heading2");
+        item2.setIconImg(android.R.drawable.ic_delete);
+        listDataHeader.add(item2);
+
+        ExpandedMenuModel item3 = new ExpandedMenuModel();
+        item3.setIconName("heading3");
+        listDataHeader.add(item3);
+
+        // Adding child data
+        List<String> heading1 = new ArrayList<String>();
+        heading1.add("Submenu of item 1");
+
+        List<String> heading2 = new ArrayList<String>();
+        heading2.add("Submenu of item 2");
+        heading2.add("Submenu of item 2");
+        heading2.add("Submenu of item 2");
+
+        List<String> heading3 = new ArrayList<String>();
+        heading3.add("hi");
+
+        listDataChild.put(listDataHeader.get(0), heading1);// Header, Child data
+        listDataChild.put(listDataHeader.get(1), heading2);
+        listDataChild.put(listDataHeader.get(2), heading3); */
+
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        //revision: this don't works, use setOnChildClickListener() and setOnGroupClickListener() above instead
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
 }
